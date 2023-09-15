ef02e833ccbd969e02c2947744ab8b9d670d63a4
ninotme <80800793+ninotme@users.noreply.github.com>
2023-09-15 18:48:11 +0200

    Update pgpe.py

--- pgpe.py
+++ pgpe.py
@@ -1,123 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  4 18:13:18 2018
+
+@author: matteo
+"""
+"""References
+    PGPE: Sehnke, Frank, et al. "Policy gradients with parameter-based exploration for
+        control." International Conference on Artificial Neural Networks. Springer,
+        Berlin, Heidelberg, 2008.
+    Optimal baseline: Zhao, Tingting, et al. "Analysis and
+        improvement of policy gradient estimation." Advances in Neural
+        Information Processing Systems. 2011.
+"""
+
 import numpy as np
-from gymnasium import spaces
-class HyperPolicy:
-    def __init__(self, param_number, alpha=0.5):
-
-        self.alpha = alpha
-        self.param_number = param_number
-
-        # Inizializzazione della distribuzione iperparamentrica
-        self.mu= [ 0.5 for i in range(self.param_number)]
-        self.sigma = [0.5 for i in range(self.param_number)]
-
-        self.theta = self.act(0)
-        print("done initializing")
-
-    # Called by algorithm
-    def act(self, state):
-        return self.theta
-        pass
-
-    #calcolo del delta_rho
-    def eval_gradient(self, actor_params, disc_returns):
-        N = len(disc_returns)
-
-        sum_sigma = [0 for _ in len(actor_params[0])]
-        sum_mu = [0 for _ in len(actor_params[0])]
+#from baselines import logger
 
 
-        for i in range(N):
-            mus = []
-            sigmas = []
-            for j in actor_params[i]:
-                theta = actor_params[i][j]
-                score_mu = (theta - self.mu[j]) / self.sigma[j] ** 2
-                score_sigma = (theta - self.mu[j])**2 - self.sigma[i]/ (self.sigma[j] ** 3)
+def eval_trajectory(env, pol, gamma, task_horizon, feature_fun):
+    ret = disc_ret = 0
+    
+    t = 0
+    ob = env.reset()
+    done = False
+    while not done and t<task_horizon:
+        s = feature_fun(ob) if feature_fun else ob
+        a = pol.act(s)
+        ob, r, trunc, term,  _ = env.step(a)
+        ret += r
+        print("immediate reward : ",r)  
+        disc_ret += gamma**t * r
+        t+=1
+        done = trunc or term
+    if done: 
+        print("raggiunto la fine!!!!") 
+        env.reset()
+        
+    return ret, disc_ret, t
+        
 
-                #multiplication for the disc
-                score_mu = score_mu * disc_returns[i]
-                mus.append(score_mu)
-                score_sigma = score_sigma * disc_returns[i]
-                sigmas.append(score_sigma)
-            sum_mu = np.add(sum_mu, mus)
-            sum_sigma = np.add(sum_sigma, sigmas)
+def learn(env, pol, gamma, step_size, batch_size, task_horizon, max_iterations, 
+          feature_fun=None, use_baseline=True, step_size_strategy=None, 
+          verbose=True, 
+          save_to=None):
+    
+    #Logging
+    format_strs = []
+    if verbose: format_strs.append('stdout')
+    if save_to: format_strs.append('csv')
+    #logger.configure(dir=save_to, format_strs=format_strs)
 
-        grad_mu = np.divide(sum_mu, N)
-        grad_sigma = np.divide(sum_sigma, N)
-        return [grad_mu, grad_sigma]
-
-        pass
-        score = []
-        for i in range(len(disc_returns)):
-            theta_i = actor_params[i]
-            diff = np.multiply(np.subtract(theta_i, self.rho), disc_returns[i])
-            score.append(diff)
-
-        #calcolo di delta_mu
-        delta_rho =[0 for i in range(len(score[0]))]
-
-        for j in range(len(delta_rho)):
-            for i in range(len(score)):
-                delta_rho[j] = score[i][j] + delta_rho[j]
-
-
-
-
-       # grad = np.sum()
-        return np.divide(delta_rho, len(disc_returns))
-
-    def eval_param(self):
-        return self.rho
-    def set_params(self,rho):
-        self.rho = rho
-        pass
-
-    def resample(self):
-        return [
-            np.random.normal(self.mu[i], self.sigma[i]) for i in range(self.param_number)
-        ]
-
-    #internal methods
-    def set_rho(self, mu, sigma):
-        self.mu = mu
-        self.sigma = sigma
-    def get_rho(self):
-        return [self.mu, self.sigma]
-
-    def set_theta(self, theta):
-        self.theta = theta
-    def get_theta(self):
-        return self.theta
-
-
-    ## Unused methods
-
-    def renyi(self, pol):
-        pass
-    def eval_fisher(self):
-        pass
-
-#test con finta reward_batch [1000, 1000]
-# il batch di  parametri della policy di test campionati dalla policy
-# degli iperparamenteri sono 2:  [theta1, theta2]
-if __name__ == '__main__':
-
-    hp = HyperPolicy(3)
-
-    # prelevo due campioni dalla attuale distribuzione degli iperparametri
-    theta1 = hp.resample()
-    theta2 = hp.resample()
-
-    #test update
-    actor_params = [theta1, theta2]
-    disc_returns = [1000, 1000]
-
-    # valutazione del gradiente
-    grad  = hp.eval_gradient(actor_params, disc_returns)
-
-    #update della distribuzione degli iperparamentri
-    step_size = 0.7
-    delta_rho = np.multiply(grad, step_size)
-    hp.set_params(hp.get_rho() + delta_rho)
-    thetaf = hp.resample()
-    print(f'fine')
+    
+    #Learning iteration
+    for it in range(max_iterations):
+        rho = pol.eval_params() #Higher-order-policy parameters
+        if save_to: np.save(save_to + '/weights_' + str(it), rho)
+            
+        #Batch of episodes
+        #TODO: try symmetric sampling
+        actor_params = []
+        rets, disc_rets, lens = [], [], []
+        
+        # populating theta!s! and their respective discounted return
+        for ep in range(batch_size):
+            theta = pol.resample()
+            actor_params.append(theta)
+            ret, disc_ret, ep_len = eval_trajectory(env, pol, gamma, task_horizon, feature_fun)
+            rets.append(ret)
+            disc_rets.append(disc_ret)
+            lens.append(ep_len)
+            
+        #logger.log('\n********** Iteration %i ************' % it)
+        if verbose>1:
+            print('Higher-order parameters:', rho)
+            #print('Fisher diagonal:', pol.eval_fisher())
+            #print('Renyi:', pol.renyi(pol))
+        #logger.record_tabular('AvgRet', np.mean(rets))
+        #logger.record_tabular('J', np.mean(disc_rets))
+        #logger.record_tabular('VarJ', np.var(disc_rets, ddof=1)/batch_size)
+        #logger.record_tabular('BatchSize', batch_size)
+        #logger.record_tabular('AvgEpLen', np.mean(lens))
+        
+        #Update higher-order policy
+        grad = pol.eval_gradient(actor_params, disc_rets, use_baseline=use_baseline)
+        if verbose>1:
+            print('grad:', grad)
+            
+        grad2norm = np.linalg.norm(grad, 2)
+        gradmaxnorm = np.linalg.norm(grad, np.infty)
+        
+        step_size_it = {'const': step_size,
+                        'norm': step_size/grad2norm if grad2norm>0 else 0,
+                        'vanish': step_size/np.sqrt(it+1)
+                }.get(step_size_strategy, step_size)
+        
+        print("--------IN LEARN()-_--_______") 
+        print("step_size: ", step_size_it) 
+        print("grad: ", grad) 
+        print("grad[0]: ", grad[0])
+        print("grad[1]: ", grad[1]) 
+        
+        delta_rho = [step_size_it * grad[0], 
+                     step_size_it * grad[1]]
+        print("delta_rho: ", delta_rho)
+        
+        #update mu 
+        #hardcoded because stupid python typesystem
+        update = []
+        for i in range(len(rho)):
+            update.append(delta_rho[i] + rho[i]) 
+        test = [rho[0] + delta_rho[0], rho[1] + delta_rho[1]]
+        print("update: ", update) 
+        
+        print("rho+delta_rho = ", test) 
+        
+        
+        pol.set_params(test)
+        
+       #logger.record_tabular('StepSize', step_size_it)
+        #logger.record_tabular('GradInftyNorm', gradmaxnorm)
+       # logger.record_tabular('Grad2Norm', grad2norm) 
+       # logger.dump_tabular()
+        
+    
