0272f739900c9c2f7e1178cd0baf5f8ab57ec893
ninotme <wgiuseppe.t@gmail.com>
2023-10-11 23:58:47 +0200

    moves file in the correct directory

---
+++ pgpe-gym/logger/log_test.py
@@ -0,0 +1,10 @@
+import logging
+
+
+logging.debug("A debug message")
+logging.info("An info message")
+
+# default print downward here 
+logging.warning("A warning message")
+logging.error("An error message")
+logging.critical("A critical message")
